20d2efae56f76b698005f7f721b4aec60c09ba74
Jay <jay.feldman03@gmail.com>
2025-03-08 00:35:39 -0600

    Update variable name used

--- pages/api/utils/awsS3.ts
+++ pages/api/utils/awsS3.ts
@@ -11,7 +11,7 @@ const writeFileAsync = promisify(writeFile);
 
 // Configure AWS SDK
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
